24c1c7a033de5efe8c45d0a13a32674e1ef03c40
roicenir <roicenir2@hotmail.com>
2019-10-03 11:01:11 -0300

    b

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = ["/", "/styles/main.css", "/script/main.js"];
+
 // https://developers.google.com/web/fundamentals/primers/service-workers/?hl=pt-br
 self.addEventListener("install", function(event) {
   // Perform install steps
@@ -63,16 +64,55 @@ self.addEventListener("activate", function(event) {
   );
 });
 
-fetch(url, {
-  credentials: "include"
-});
+//fetch(url, {
+//  credentials: "include"
+//});
 
-cache
-  .addAll(
-    urlsToPrefetch.map(function(urlToPrefetch) {
-      return new Request(urlToPrefetch, { mode: "no-cors" });
-    })
-  )
-  .then(function() {
-    console.log("All resources have been fetched and cached.");
-  });
+//cache.addAll(
+//    urlsToPrefetch.map(function(urlToPrefetch) {
+//      return new Request(urlToPrefetch, { mode: "no-cors" });
+//    })
+//  )
+//  .then(function() {
+//    console.log("All resources have been fetched and cached.");
+//  });
+
+// CODELAB: Update cache names any time any of the cached files change.
+const FILES_TO_CACHE = [
+  '/offline.html',
+];
+
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+  caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
+  })
+);
+
+// CODELAB: Remove previous cached data from disk.
+evt.waitUntil(
+  caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key !== CACHE_NAME) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        return caches.delete(key);
+      }
+    }));
+  })
+);
+
+// CODELAB: Add fetch event handler here.
+if (evt.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+evt.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
+);
