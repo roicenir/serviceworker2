0a9bec52bcf8faefbb7a4d1c05e0ac2f820f0b3e
roicenir <roicenir2@hotmail.com>
2019-10-16 10:58:16 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var worker = new Worker('doWork2.js');
 
-worker.addEventListener('message', function(e) {
+worker.addEventListener('message', function (e) {
   console.log('Worker said: ', e.data);
 }, false);
 
@@ -19,20 +19,51 @@ document.getElementById("app").innerHTML = `
 
 // REGISTRO DO SW
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("sw.js").then(
-      function(registration) {
-        // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function(err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
+//if ("serviceWorker" in navigator) {
+//  window.addEventListener("load", function() {
+//    navigator.serviceWorker.register("sw.js").then(
+//      function(registration) {
+//        // Registration was successful
+//        console.log(
+//          "ServiceWorker registration successful with scope: ",
+//          registration.scope
+//       );
+//      },
+//      function(err) {
+//        // registration failed :(
+//       console.log("ServiceWorker registration failed: ", err);
+//      }
+//    );
+//  });
+//}
+
+if ('serviceWorker' in navigator) {
+  // observe messages
+  navigator.serviceWorker.addEventListener('message', event => {
+    if ('cmd' in event.data) {
+      let command = event.data.cmd
+      switch (command) {
+        case 'response':
+          console.log('[ServiceWorker Response]: ', event.data.data)
+          break
       }
-    );
-  });
+    }
+  })
+  // simple lógic
+  setInterval(() => {
+    if (navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage({
+        cmd: 'executa-funcao'
+      })
+    }
+  }, 5000) // every 5 seconds
+
+  // register
+  navigator.serviceWorker.register('/sw.js', {
+    scope: '/'
+  }).then(register => {
+    console.log('[Register ServiceWorker] We are live 🚀 !')
+  }).catch(e => {
+    console.warn('service worker failed', e)
+  })
 }
--- sw.js
+++ sw.js
@@ -66,55 +66,25 @@ self.addEventListener("activate", function(event) {
   );
 });
 
-//fetch(url, {
-//  credentials: "include"
-//});
-
-//cache.addAll(
-//    urlsToPrefetch.map(function(urlToPrefetch) {
-//      return new Request(urlToPrefetch, { mode: "no-cors" });
-//    })
-//  )
-//  .then(function() {
-//    console.log("All resources have been fetched and cached.");
-//  });
-
-// CODELAB: Update cache names any time any of the cached files change.
-//const FILES_TO_CACHE = [
-//  '/offline.html',
-//];
-
-// CODELAB: Precache static resources here.
-//evt.waitUntil(
-//  caches.open(CACHE_NAME).then((cache) => {
-//  console.log('[ServiceWorker] Pre-caching offline page');
-//return cache.addAll(FILES_TO_CACHE);
-//  })
-//);
-
-// CODELAB: Remove previous cached data from disk.
-//evt.waitUntil(
-// caches.keys().then((keyList) => {
-//  return Promise.all(keyList.map((key) => {
-//    if (key !== CACHE_NAME) {
-//       console.log('[ServiceWorker] Removing old cache', key);
-//       return caches.delete(key);
-//     }
-//   }));
-// })
-//);
-
-// CODELAB: Add fetch event handler here.
-//if (evt.request.mode !== 'navigate') {
-// Not a page navigation, bail.
-//  return;
-//}
-//evt.respondWith(
-//    fetch(evt.request)
-//        .catch(() => {
-//          return caches.open(CACHE_NAME)
-//              .then((cache) => {
-//               return cache.match('offline.html');
-//             });
-//       })
-//);
+function ExecutaFuncao() {
+  postMessage({
+      cmd: 'response',
+      data: 'um valor qualquer'
+  })
+}
+// message handler
+self.addEventListener('message', event => {
+  if ( 'cmd' in event.data ) {
+      let command = event.data.cmd
+      switch (command) {
+          case 'executa-funcao':
+              ExecutaFuncao()
+          break
+      }
+  }
+})
+// enviar mensagem do Service Worker
+async function postMessage(data) {
+  let clients = await self.clients.matchAll()
+  clients.forEach(client => client.postMessage(data))
+}
