dd5c0b1d5ed375a21d10c393334a84420e053cc0
roicenir <roicenir2@hotmail.com>
2019-10-16 00:10:10 -0300

    a

--- doWork.js
+++ doWork.js
@@ -2,4 +2,4 @@ self.addEventListener('message', function(e) {
     self.postMessage(e.data);
   }, false);
 
-  https://www.html5rocks.com/pt/tutorials/workers/basics/
+ // https://www.html5rocks.com/pt/tutorials/workers/basics/
