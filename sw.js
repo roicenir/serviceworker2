41e50bfcd44f486b5c47048f632bda662fc8d963
roicenir <roicenir2@hotmail.com>
2019-10-17 10:34:36 -0300

    a

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+import * as workerTimers from 'worker-timers';
+
 var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = ["/", "/styles.css", "/index.js"];
 
@@ -52,12 +54,13 @@ self.addEventListener("fetch", function(event) {
 
 self.addEventListener("activate", function(event) {
   var cacheWhitelist = ["pages-cache-v1", "blog-posts-cache-v1"];
- 
+  
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            setTimeout(0);
             return caches.delete(cacheName);
           }
         })
